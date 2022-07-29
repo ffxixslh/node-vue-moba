b22d02971f77f65765280961b9bebd040fd83f4a
luohao <1223048059@qq.com>
2022-07-29 16:44:22 +0800

    新增 权限拦截、jwt；修改 后端返回用data封装；学习 中间件抽离

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,69 +1,111 @@
 // 通用crud接口
 module.exports = (app) => {
   const express = require("express");
+  const jwt = require("jsonwebtoken");
+  const AdminUser = require("../../models/AdminUser");
+  const assert = require("http-assert");
   const router = express.Router({
     mergeParams: true,
   });
 
+  // 创建资源
   router.post("/", async (req, res) => {
     const model = await req.Model.create(req.body);
-    res.send(model);
+    res.send({ data: model });
   });
+
+  // 更新资源
   router.put("/:id", async (req, res) => {
     const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
-    res.send(model);
+    res.send({ data: model });
   });
+
+  // 删除资源
   router.delete("/:id", async (req, res) => {
     const model = await req.Model.findByIdAndDelete(req.params.id, req.body);
     res.send({
-      success: true,
+      data: {
+        success: true,
+      },
     });
   });
+
+  // 资源列表
   router.get("/", async (req, res) => {
     const queryOptions = {};
     if (req.Model.modelName === "Category") {
       queryOptions.populate = "parent";
     }
     const items = await req.Model.find().setOptions(queryOptions).limit(10);
-    res.send(items);
+    res.send({ data: items });
   });
+
+  // 资源详情
   router.get("/:id", async (req, res) => {
     const model = await req.Model.findById(req.params.id);
-    res.send(model);
+    res.send({ data: model });
   });
+
   /**
-   * req.Model.collection.deleteMany ({})
    * 删除全部数据
    */
+  function deleteMany() {
+    req.Model.collection.deleteMany({});
+  }
+
+  // 登录校验中间件
+  const authMiddleWare = require("../../middleware/auth.js");
+
+  // 资源子组件
+  const resourceMiddleWare = require("../../middleware/resource.js");
+
   app.use(
     "/admin/api/rest/:resource",
-    async (req, res, next) => {
-      const modelName = require("inflection").classify(req.params.resource);
-      req.Model = require(`../../models/${modelName}`);
-      next();
-    },
+    authMiddleWare(),
+    resourceMiddleWare(),
     router
   );
 
   const multer = require("multer");
   const upload = multer({ dest: __dirname + "/../../uploads" });
-  app.post("/admin/api/upload", upload.single("file"), async (req, res) => {
-    const file = req.file;
-    file.url = `http://localhost:3000/uploads/${file.filename}`;
-    res.send(file);
-  });
+  app.post(
+    "/admin/api/upload",
+    authMiddleWare(),
+    upload.single("file"),
+    async (req, res) => {
+      const file = req.file;
+      file.url = `http://localhost:3000/uploads/${file.filename}`;
+      res.send({ data: file });
+    }
+  );
+
+  app.set("secret", "1234567890abcdefg");
 
   app.post("/admin/api/login", async (req, res) => {
     const { username, password } = req.body;
     // 1.根据用户名找用户
-    const AdminUser = require("../../models/AdminUser");
-    const user = await AdminUser.findOne({ username });
-    if (!user) {
-      return res.status(422).send({
-        message: "用户不存在",
-      });
-    }
+    const user = await AdminUser.findOne({ username }).select("+password");
+    /**
+     * 将
+     * if (!variable) {
+     *   return res.status(statusCode).send({
+     *     message: "msg",
+     *   });
+     * }
+     * 替换为assert(variable, statusCode, "msg")
+     */
+    assert(user, 422, "用户不存在");
     // 2.校验密码
-    // 3.生成token
+    const isValid = require("bcrypt").compareSync(password, user.password);
+    assert(isValid, 422, "密码错误");
+    // 3.返回token
+    const token = jwt.sign({ id: user._id }, app.get("secret"));
+    res.send({ data: { token } });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+      message: err.message,
+    });
   });
 };
