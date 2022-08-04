bad5c56de27b6287e8c6e1d9c579b655b9df4db3
luohao <1223048059@qq.com>
2022-08-04 15:21:28 +0800

    新增 打包配置

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -17,9 +17,38 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  esbuild: {
+    pure: ["console.log"],
+    minify: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    // 指定小于多少大小的内容将内联为base64，减少请求次数（默认4096，即4kb）
+    assetsInlineLimit: 4096,
+    // 构建后是否生成source map文件，默认为false
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    manifest: true,
+  },
+
   server: {
     port: 8080,
     // open: true,
-    hmr: true,
+    // hmr: true,
   },
 });
